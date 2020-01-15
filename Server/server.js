11f3c3ea6906aab4da034dd7b7dff6d565a6eb4c
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-15 13:25:26 -0700

    front end displays post, delete post functional. need to add handle change for add post.

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const Router = require("../Router/router.js");
 const server = express();
 server.use(express.json());
-
+server.use(cors());
 server.use("/api/posts", Router);
 module.exports = server;
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = () => {
+      axios.get("http://ctfjmg01:4000/api/posts").then(res => {
+        setPosts(res.data);
+      });
+    };
+    getPosts();
+  }, []);
+  const addPost = (title, contents) => {
+    const values = {
+      titile: title,
+      bio: contents
+    };
+    axios.post("http://ctfjmg01:4000/api/posts", values);
+  };
+  const deletepost = id => {
+    axios.delete(`http://ctfjmg01:4000/api/posts/${id}`).then(res => {
+      axios.get("http://ctfjmg01:4000/api/posts").then(res => {
+        setPosts(res.data);
+      });
+    });
+  };
+  const handleChange = () => {};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add Post:</h1>
+      <div className="addPost">
+        <p> Title: </p>
+        <input
+          type="text"
+          id="posttitle"
+          name="posttitle"
+          onChange={() => handleChange()}
+        ></input>
+        <p> Contents: </p>
+        <input
+          type="text"
+          id="postcontents"
+          name="postcontents"
+          onChange={() => handleChange()}
+        ></input>
+        <button onClick={() => addPost()}> Add </button>
+      </div>
+      <h1> Posts: </h1>
+      <div className="postBox">
+        {posts.map(post => (
+          <div className="postCard" key={post.id}>
+            <h3> {post.title} </h3> <p> {post.contents} </p>
+            <button onClick={() => deletepost(post.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
